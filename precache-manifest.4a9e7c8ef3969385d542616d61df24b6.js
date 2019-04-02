cd82fb8431443e5245eeecb520b5bacca0c381ba
Shahzad <mshahzadreza@gmail.com>
2019-04-02 17:03:36 +0600

    Updates

---
+++ precache-manifest.4a9e7c8ef3969385d542616d61df24b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3dfcd357096020e7f842",
+    "url": "/react.contact/static/js/runtime~main.7dd43d1b.js"
+  },
+  {
+    "revision": "c741cb2d750f92c15cb0",
+    "url": "/react.contact/static/js/main.cd9cad5b.chunk.js"
+  },
+  {
+    "revision": "7e3e7443cf919f1ace7a",
+    "url": "/react.contact/static/js/2.74ff0e9a.chunk.js"
+  },
+  {
+    "revision": "c741cb2d750f92c15cb0",
+    "url": "/react.contact/static/css/main.e6257bfd.chunk.css"
+  },
+  {
+    "revision": "7e3e7443cf919f1ace7a",
+    "url": "/react.contact/static/css/2.0e15a8e5.chunk.css"
+  },
+  {
+    "revision": "0ce1f1bf0cfff848af00f2602c60c03d",
+    "url": "/react.contact/index.html"
+  }
+];
